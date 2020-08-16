59b50d78c2bf1480efa32f58181d3a4383299763
Jason Thorpe <jdthorpe@gmail.com>
2020-08-16 14:19:04 -0700

    better types?

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/// <reference path="types.d.ts"/>
 var extract_module_details_1 = require("./src/extract-module-details");
 exports.parse_module = extract_module_details_1.parse_module;
 var module_parser_1 = require("./module-parser");
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 /// <reference path="types.d.ts"/>
+export * from "./types";
 export { parse_module } from "./src/extract-module-details";
 export { parse } from "./module-parser";
